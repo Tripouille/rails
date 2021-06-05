2653a9530a121936bab1876edfaf27b66836eab4
Tripouille <jmichel.gambard@gmail.com>
2021-06-05 23:42:05 +0200

    add data

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mkdir -p pg_commit_ts pg_dynshmem pg_logical/mappings pg_logical/snapshots pg_replslot \
+pg_serial pg_snapshots pg_stat pg_stat_tmp pg_tblspc pg_twophase pg_wal/archive_status
 chown -R postgres:postgres /var/lib/postgresql/11/main
 chmod -R 700 /var/lib/postgresql/11/main
 service postgresql start
