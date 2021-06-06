aaf132a0393d3905bf60a68c516b1d91c7362f70
Tripouille <jmichel.gambard@gmail.com>
2021-06-06 22:16:54 +0200

    working on mode production

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cd /var/lib/postgresql/11/main
 mkdir -p pg_commit_ts pg_dynshmem pg_logical/mappings pg_logical/snapshots pg_replslot \
 pg_serial pg_snapshots pg_stat pg_stat_tmp pg_tblspc pg_twophase pg_wal/archive_status
@@ -8,4 +8,6 @@ service postgresql start
 psql -U postgres -c "ALTER USER admin WITH password '${ADMIN_PASSWORD}'"
 cd /www
 bundle install
+rm -f /www/tmp/pids/server.pid
+if [ "$RAILS_ENV" == "production" ]; then rails assets:precompile; fi
 rails s -b "0.0.0.0"
